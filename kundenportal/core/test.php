86a2a650d458d5e90709d1cc5bb85cea5b281954
RudenokArt <rudenokart@yandex.ru>
2023-09-15 12:39:45 +0300

    save

---
+++ kundenportal/core/test.php
@@ -0,0 +1,17 @@
+<?php 
+
+$arr = [
+	[
+		'tile' => 'implementation',
+		'price' => [
+			[50, 475, 'E'],
+			[100, 575],
+			[200, 1025],
+		],
+		'part' => 'learning/content',
+		'type' => 'license/flat',
+
+	],
+];
+
+?>
--- page-new_review.php
+++ page-new_review.php
@@ -1,5 +1,7 @@
 <?php include_once 'kundenportal/init.php';?>
 
+<pre><?php // print_r($B24_CONTACT); ?></pre>
+
 <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
 <div id="google_search-instance" class="pb-5">
@@ -62,6 +64,7 @@
 	</form>
 
 	<div v-if="outscraperPreloader">
+		<p>Bewertungen werden analysiert... <br>Das kann ein paar minuten dauern...</p>
 		<p class="placeholder-glow p-2 border-bottom">
 			<span class="placeholder col-5 bg-secondary placeholder-lg"></span>
 			<span class="placeholder col-10 bg-secondary placeholder-lg"></span>
@@ -80,85 +83,94 @@
 		</p>
 	</div>
 	<div v-if="outscraperResponse && !outscraperPreloader">
-			<div class="pt-5">
-		<div class="card">
-			<div class="card-header">
-				<div class="row">
-					<div class="col-lg-4 col-md-4 col-sm-12 text-center">
-						<img v-bind:src="outscraperData.logo" width="200" alt="">
-					</div>
-					<div class="col-lg-8 col-md-8 col-sm-12">
-						<div>
-							<a v-bind:href="outscraperData.site" target="_blank" class="text-decoration-none">
-								<b>{{outscraperData.name}}</b>
-								{{outscraperData.country}}
-								{{outscraperData.city}}
-							</a>	
+		<div class="pt-5">
+			<div class="card">
+				<div class="card-header">
+					<div class="row">
+						<div class="col-lg-4 col-md-4 col-sm-12 text-center">
+							<img v-bind:src="outscraperData.logo" width="200" alt="">
 						</div>
-						<div>{{outscraperData.street}}</div>
-						<div>{{outscraperData.phone}}</div>
-						<div>{{outscraperData.site}}</div>
-						<div>{{outscraperData.full_address}}</div>
-					</div>
-					
-				</div>
-				<div>{{outscraperData.query}}</div>
-			</div>
-			<div class="card-body">
-				<div class="row pt-3" v-for="item, index in outscraperData.reviews_data">
+						<div class="col-lg-8 col-md-8 col-sm-12">
+							<div>
+								<a v-bind:href="outscraperData.site" target="_blank" class="text-decoration-none">
+									<b>{{outscraperData.name}}</b>
+									{{outscraperData.country}}
+									{{outscraperData.city}}
+								</a>	
+							</div>
+							<div>{{outscraperData.street}}</div>
+							<div>{{outscraperData.phone}}</div>
+							<div>{{outscraperData.site}}</div>
+							<div>{{outscraperData.full_address}}</div>
+						</div>
 
-					<div class="col-lg-3 col-md-3 col-sm-12">
-						<img v-bind:src="item.author_image" alt="" class="h-100"><br>
 					</div>
-					<div class="col-lg-6 col-md-6 col-sm-12">
-						{{item.review_id}}<br>
-						{{item.author_title}}<br>
-						<span v-for="num in 5" class="text-warning">
-							<template v-if="num <= item.review_rating">
-								<i class="fa fa-star" aria-hidden="true"></i>
-							</template>
-							<template v-else="num > item.review_rating">
-								<i class="fa fa-star-o" aria-hidden="true"></i>
-							</template>
-						</span>
-						<span v-if="item.review_rating">
-							({{item.review_rating}})
-						</span>
-						<br>
-						<a v-bind:href="item.review_link" target="_blank">In der Quelle</a><br>
-					</div>
-
-					<div class="col-lg-3 col-md-3 col-sm-12">
-						<div class="form-check">
-							<label class="form-check-label">
-								<input class="form-check-input" type="checkbox" v-bind:value="item.review_id" name="need" v-on:change="needAndReasonsSet">
-								Zum Löschen senden
-							</label>
-						</div>
-						<div class="pt-2" v-if="need.includes(item.review_id)">
-							<select class="form-select" name="reasons" v-on:change="needAndReasonsSet">
-								<option value="-" selected>Select reason</option>
-								<option value="No text">No text</option>
-								<option value="Fake name">Fake name</option>
-								<option value="Bullshit">Bullshit</option>
-								<option value="Spam">Spam</option>
-								<option value="Offense">Offense</option>
-								<option value="Unacceptable content">Unacceptable content</option>
-								<option value="Advertisement">Advertisement</option>
-								<option value="Other">Other</option>
-							</select>
-						</div>
-					</div>
-					<div class="col-12 border-bottom pb-3">
-						{{item.review_text}}<br>
-					</div>
+					<div>{{outscraperData.query}}</div>
 				</div>
-				<div class="text-center pt-5 pb-5">
-					<button class="w-75 btn-lg btn-primary" v-on:click="taskAddFromSite">Send</button>
+				<div class="card-body">
+					<div class="row pt-3" v-for="item, index in outscraperData.reviews_data">
+
+						<div class="col-lg-3 col-md-3 col-sm-12">
+							<img v-bind:src="item.author_image" alt="" class="h-100"><br>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-12">
+							{{item.review_id}}<br>
+							{{item.author_title}}<br>
+							<span v-for="num in 5" class="text-warning">
+								<template v-if="num <= item.review_rating">
+									<i class="fa fa-star" aria-hidden="true"></i>
+								</template>
+								<template v-else="num > item.review_rating">
+									<i class="fa fa-star-o" aria-hidden="true"></i>
+								</template>
+							</span>
+							<span v-if="item.review_rating">
+								({{item.review_rating}})
+							</span>
+							<br>
+							<a v-bind:href="item.review_link" target="_blank">In der Quelle</a><br>
+						</div>
+
+						<div class="col-lg-3 col-md-3 col-sm-12">
+							<div class="form-check">
+								<label class="form-check-label">
+									<input class="form-check-input" type="checkbox" v-bind:value="item.review_id" name="need" v-on:change="needAndReasonsSet">
+									Zum Löschen senden
+								</label>
+							</div>
+							<div class="pt-2" v-if="need.includes(item.review_id)">
+								<select class="form-select" name="reasons" v-on:change="needAndReasonsSet">
+									<option value="-" selected>Select reason</option>
+									<option value="No text">No text</option>
+									<option value="Fake name">Fake name</option>
+									<option value="Bullshit">Bullshit</option>
+									<option value="Spam">Spam</option>
+									<option value="Offense">Offense</option>
+									<option value="Unacceptable content">Unacceptable content</option>
+									<option value="Advertisement">Advertisement</option>
+									<option value="Other">Other</option>
+								</select>
+							</div>
+						</div>
+						<div class="col-12 border-bottom pb-3">
+							{{item.review_text}}<br>
+						</div>
+					</div>
+					<div class="text-center pt-5 pb-5">
+						<div class="pb-2">
+							<input type="text" class="form-control" v-model="userName">
+						</div>
+						<div class="pb-2">
+							<input type="text" class="form-control" v-model="userPhone">
+						</div>
+						<div class="pb-3">
+							<input type="text" class="form-control" v-model="userEmail">
+						</div>
+						<button class="w-75 btn-lg btn-primary" v-on:click="taskAddFromSite">Send</button>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
 	</div>
 
 
@@ -185,12 +197,12 @@
 				outscraperData: false,
 				need: [],
 				reasons: [],
+				userName: '<?php echo $B24_CONTACT->data["NAME"]." ".$B24_CONTACT->data["SECOND_NAME"]." ".$B24_CONTACT->data["LAST_NAME"];?>',
+				userPhone: '<?php echo $B24_CONTACT->data["PHONE"][0]["VALUE"];?>',
+				userEmail: '<?php echo $B24_CONTACT->data["EMAIL"][0]["VALUE"];?>',
 			};
 		},
 
-		// mounted: async function (){
-		// 	this.outscraperData = await this.getTestReviews();
-		// },
 
 		watch: {
 			searchProcess: function () {
@@ -210,7 +222,7 @@
 					reasons: JSON.stringify(this.reasons),
 					price: 100, 
 					amount: 300,
-					uName: 'contactName',
+					uName: this.userName,
 					uPhone: '55-555-555',
 					uEmail: 'email@mail.ru',
 				});
@@ -236,13 +248,6 @@
 					}
 				}, 100);
 				
-			},
-
-			getTestReviews: async function () {
-				var re = await $.get(this.ajaxUrl+'?test=Y');
-				var re1 = JSON.parse(re);
-				var re2 = re1.result.result;
-				return re2[0];
 			},
 
 			requestOutscraper: async function (e) {
